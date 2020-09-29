864357572b297cddbc9a97ac435dbe8499e527ed
Matija Osojnik <matija1.osojnik@gmail.com>
2020-09-29 21:11:20 +0200

    Update PG Connection URL

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,13 +3,21 @@ require('dotenv').config()
 module.exports = {
     port: process.env.PORT || 8082,
     db: {
-        database_url: process.env.DB_URI,
+        database: process.env.DB_PROD_NAME || 'medio',
+        username: process.env.DB_PROD_USERNAME || 'postgres',
+        password: process.env.DB_PROD_PASSWORD || 'postgres',
         options: {
-            dialect: process.env.DIALECT || 'postgres'
+            dialect: process.env.DIALECT || 'postgres',
+            host: process.env.HOST || 'localhost',
         }
     },
     authentication: {
-        sessionSecret: process.env.SESSION_SECRET,
-        jwtSecret: process.env.JWT_SECRET
+        jwtSecret: process.env.JWT_SECRET || 'secret',
+        facebookApiKey: process.env.FACEBOOK_API_KEY,
+        facebookApiSecret: process.env.FACEBOOK_API_SECRET,
+        facebookCallbackURL: process.env.FACEBOOK_API_CALLBACK_URL,
+        googleApiKey: process.env.GOOGLE_API_KEY,
+        googleApiSecret: process.env.GOOGLE_API_SECRET,
+        googleCallbackURL: process.env.GOOGLE_API_CALLBACK_URL,
     }
 }
