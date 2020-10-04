1a9143e22d88cdb453aeed6ede87b3babb0b7ba4
Matija Osojnik <matija1.osojnik@gmail.com>
2020-10-04 18:57:53 +0200

    Small Bug Fixes

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -118,6 +118,7 @@ export default {
           email: this.email,
           password: this.password,
           repeat_password: this.repeat_password,
+          roles: ["Admin"]
         });
         this.showPanel = true;
 
